aedf7fd464c5ac87fef2031794a96bdf9e9dc14c
Hello Gorgeous <hellogorgeous@Hello-Gorgeous.local>
2020-06-03 21:41:42 -0400

    working on the logic to solve leap year for year 2100, not working yet

--- leap_year_h.py
+++ leap_year_h.py
@@ -1,15 +1,28 @@
 def is_leap(year):
     leap = False
     
+    year = 2100
+    print(year, year %100)
     # Write your logic here
-    if year %4 == 0 :
-        leap = True
-    elif year %100 == 0 :
+    if year %100 == 0 :
         leap = False
+    elif year %4 == 0 :
+        leap = True
+    
+        
     elif year %400 == 0 :
         leap = True
     
     
+    #     if year %4 == 0 :
+    #     leap = True
+    # elif year %100 == 0 :
+    #     leap = False
+        
+    # elif year %400 == 0 :
+    #     leap = True
+
+
     return leap
 
 year = int(input())
